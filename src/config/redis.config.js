332e491195d8b177c65d092b9f7956eb829801f1
EkeneFidel <echukwurah99@gmail.com>
2023-06-21 11:01:13 +0100

    first commit

--- src/config/redis.config.js
+++ src/config/redis.config.js
@@ -1,6 +1,12 @@
 require("dotenv").config();
 const redis = require("redis");
-const redisClient = redis.createClient();
+const redisClient = redis.createClient({
+    password: process.env.REDIS_PASSW,
+    socket: {
+        host: process.env.REDIS_HOST,
+        port: process.env.REDIS_PORT,
+    },
+});
 (async () => {
     await redisClient.connect();
 })();
