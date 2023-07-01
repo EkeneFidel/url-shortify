a52c26493dd9d83f5282eb0f245470ad41bf6240
EkeneFidel <echukwurah99@gmail.com>
2023-07-01 09:42:10 +0100

    feat: add email and final functionalities

--- public/script.js
+++ public/script.js
@@ -93,9 +93,10 @@ loginForm.addEventListener("submit", async (e) => {
             .then((data) => {
                 setTimeout(() => {}, 5000);
                 if (data.success === false) {
+                    console.log(data.message);
                     stopLoad("login");
                     errorMsg[0].style.display = "flex";
-                    errorMsg[1].innerHTML = `<svg
+                    errorMsg[0].innerHTML = `<svg
                     class="leafygreen-ui-12snj1w"
                     height="15"
                     width="15"
@@ -109,11 +110,11 @@ loginForm.addEventListener("submit", async (e) => {
                         d="M8.8639 2.51357C8.49039 1.82881 7.50961 1.82881 7.1361 2.51357L1.12218 13.5388C0.763263 14.1968 1.23814 15 1.98608 15H14.0139C14.7619 15 15.2367 14.1968 14.8778 13.5388L8.8639 2.51357ZM7 6C7 5.44772 7.44772 5 8 5C8.55228 5 9 5.44772 9 6V10C9 10.5523 8.55228 11 8 11C7.44772 11 7 10.5523 7 10V6ZM9 13C9 13.5523 8.55228 14 8 14C7.44772 14 7 13.5523 7 13C7 12.4477 7.44772 12 8 12C8.55228 12 9 12.4477 9 13Z"
                         fill="currentColor"
                     ></path>
-                </svg> <p>"${data.message}"</p>`;
+                </svg> <p>${data.message}</p>`;
                     setTimeout(() => {
                         errorMsg[0].style.display = "none";
                         errorMsg[0].querySelector("p").innerHTML = "";
-                    }, 2000);
+                    }, 10000);
                 } else {
                     stopLoad("login");
                     let newURL =
--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -123,6 +123,7 @@ const login = async (req, res, next) => {
             });
         }
     } catch (error) {
+        console.log(error);
         return res.status(500).json({
             success: false,
             message: error.message,
